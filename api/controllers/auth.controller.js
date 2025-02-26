0a4a1fe8d8f0a016d8b4ceeeabffefc60e033d32
Jayson Cleve <jaysonfolorunsho@gmail.com>
2025-02-26 02:15:42 -0800

    create sign up api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,23 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Hash the password
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    // Create new user with the hashed password
+    const newUser = new User({ username, email, password: hashedPassword });
+
+    // Save the user to the database
+    await newUser.save();
+
+    // Send success response
+    res.status(201).json({ message: "User created successfully!" });
+  } catch (error) {
+    // Handle errors
+    res.status(500).json({ error: error.message });
+  }
+};
