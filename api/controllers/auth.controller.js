9fd4d177fd85ea74e760f9cbba4dc0336f892edd
Jayson Cleve <jaysonfolorunsho@gmail.com>
2025-02-28 06:39:51 -0800

    create sign api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
 
 export const signup = async (req, res, next) => {
   try {
@@ -25,3 +28,22 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const ValidUser = await User.findOne({ email });
+    if (!ValidUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, ValidUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credential!"));
+    const token = jwt.sign({ id: ValidUser._id}, process.env.JWT_SECRET)
+    const { password: pass, ...rest } = ValidUser._doc;
+    res.cookie('access_token', token, {httpOnly: true })
+    .status(300)
+    .json(rest)
+
+  } catch (error) {
+    next(error);
+  }
+}
