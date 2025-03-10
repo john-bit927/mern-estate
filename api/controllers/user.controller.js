1c7bb49a6daf7273d5773f2515c54a665cc66746
Jayson Cleve <jaysonfolorunsho@gmail.com>
2025-03-10 11:21:15 -0700

    create user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js"; // Import errorHandler
+import Listing from "../models/listing.model.js";
 
 
 
@@ -56,5 +57,20 @@ export const signOut = async (req, res, next) => {
     res.status(200).json('User has been logged out successfully!');
   } catch (error) {
    next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  
+  if (req.user.id !== req.params.id) {
+    
+    try {
+      const listings = await Listing.find({ userRef: req.params.id});
+      res.status(200).json(listings);
+    }catch (error){
+      next(error)
+    }
+  }else{
+    return next(errorHandler(401, 'You can only view your own listings'));
   }
 }
