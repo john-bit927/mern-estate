4ebe02bf3790c7ee79e3cd9b2de04f9042166969
Jayson Cleve <jaysonfolorunsho@gmail.com>
2025-03-10 13:41:37 -0700

    complete delete user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -8,3 +8,23 @@ export const createListing = async (req, res, next) => {
         next(error);
     }
 };
+
+
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found'));
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(403, 'You do not have permission to delete this listing'));
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        return res.status(200).json({ message: 'Listing deleted successfully' });
+    }catch (error) {
+        next(error);
+    }
+} 
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createListing } from '../controllers/listing.controller.js'; // ✅ Fixed import
+import { createListing, deleteListing } from '../controllers/listing.controller.js'; // ✅ Fixed import
 import { verifyToken } from '../utils/verifyUser.js'; // ✅ Ensure .js
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
+router.delete('/delete/:id', verifyToken, deleteListing);
 
 export default router; // ✅ Added missing export
